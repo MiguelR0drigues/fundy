c084642740744949fda4fd6386fff7ce8cf221b6
MiguelR0drigues <miguel.rodrigues8@hotmail.com>
2022-12-22 14:58:58 +0000

    Simplified footer

--- fundy/includes/footer.php
+++ fundy/includes/footer.php
@@ -3,15 +3,15 @@
 ?>
 
 <footer>
-    <div class="container">
+  <div class="container">
     <div class="row">
-        <div class="col-md-12">
+      <div class="col-md-12">
         <div class="inner-content">
-            <p>Copyright © 2020 Company Name - Template by: <a href="https://www.phpjabbers.com/">PHPJabbers.com</a></p>
+          <p>Copyright © 2020 Fundy</p>
         </div>
-        </div>
+      </div>
     </div>
-    </div>
+  </div>
 </footer>
 
 
