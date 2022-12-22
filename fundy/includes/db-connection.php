6944c5e72df355843a295a3a5ad96a554857823f
MiguelR0drigues <miguel.rodrigues8@hotmail.com>
2022-12-22 15:40:35 +0000

    db connectoin and get user info from DB function

---
+++ fundy/includes/db-connection.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+return $conn;
+?>
