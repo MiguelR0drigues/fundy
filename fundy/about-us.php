6f7ee4654123b5daca4af6e9bce562ea497bd31a
MiguelR0drigues <miguel.rodrigues8@hotmail.com>
2023-01-04 16:58:41 +0000

    about-us

--- fundy/about-us.php
+++ fundy/about-us.php
@@ -22,11 +22,6 @@
 <div class="best-features about-features">
   <div class="container">
     <div class="row">
-      <div class="col-md-12">
-        <div class="section-heading">
-          <h2>Lorem ipsum dolor sit amet consectetur adipisicing</h2>
-        </div>
-      </div>
       <div class="col-md-6">
         <div class="right-image">
           <img src="assets/images/about-1-570x350.jpg" alt="">
@@ -34,8 +29,8 @@
       </div>
       <div class="col-md-6">
         <div class="left-content">
-          <h4>Lorem ipsum dolor sit amet.</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed voluptate nihil eum consectetur similique? Consectetur, quod, incidunt, harum nisi dolores delectus reprehenderit voluptatem perferendis dicta dolorem non blanditiis ex fugiat. Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, consequuntur, modi mollitia corporis ipsa voluptate corrupti eum ratione ex ea praesentium quibusdam? Aut, in eum facere corrupti necessitatibus perspiciatis quis.</p>
+          <h4>What drives us?</h4>
+          <p>We started <strong>Fundy</strong> because we saw a need: no one helps great startups find talented business partners. That's why we created our web platform to help entrepreneurs and investors connect with each other, discuss projects, and discover ways they can work together to make their dreams come true.<br><br>Through our platform, entrepreneurs can present their ideas and startups, while business angels can help them navigate the waters of entrepreneurship. We give them a space where they can ask for help and connect with other like-minded people.</p>
           <ul class="social-icons">
             <li><a href="#"><i class="fa fa-facebook"></i></a></li>
             <li><a href="#"><i class="fa fa-twitter"></i></a></li>
@@ -43,25 +38,6 @@
             <li><a href="#"><i class="fa fa-behance"></i></a></li>
           </ul>
         </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-<div class="team-members">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="section-heading">
-          <h2>Lorem ipsum dolor sit amet.</h2>
-        </div>
-
-        <h5>Lorem ipsum dolor sit amet.</h5>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed voluptate nihil eum consectetur similique? Consectetur, quod, incidunt, harum nisi dolores delectus reprehenderit voluptatem perferendis dicta dolorem non blanditiis ex fugiat. Lorem ipsum dolor sit amet, consectetur adipisicing elit.<br><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, consequuntur, modi mollitia corporis ipsa voluptate corrupti eum ratione ex ea praesentium quibusdam? Aut, in eum facere corrupti necessitatibus perspiciatis quis.</p>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo quae eveniet tempora reprehenderit quo, necessitatibus vel sit laboriosam, sunt obcaecati quisquam explicabo voluptatibus earum facilis quidem fuga maiores. Quasi, obcaecati? <br><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, est officiis. Ipsam quas consequuntur adipisci quis, fuga pariatur eius eveniet qui similique nulla inventore accusantium, suscipit asperiores quibusdam culpa iure!</p>
       </div>
     </div>
   </div>
