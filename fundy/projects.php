c9745b1975e9c94dfdf07bc21c65d7f4d133bb8a
MiguelR0drigues <miguel.rodrigues8@hotmail.com>
2023-01-14 14:49:28 +0000

    Projects list page done

--- fundy/projects.php
+++ fundy/projects.php
@@ -1,7 +1,11 @@
 <?php
  include ('includes/header.php');
- include('includes/preloader.php');
+//  include('includes/preloader.php');
  include('includes/navbar.php');
+ include('includes/db-connection.php');
+ require_once('functions/get-projects.php');
+$projects = getProjects($conn);
+
 ?>
 
 
@@ -20,7 +24,8 @@
         </div>
       </div>
     </div>
-
+    <!-- TODO: Filter needs to be done -->
+    <!-- Filter -->
     <div class="products">
       <div class="container">
         <div class="row">
@@ -168,199 +173,54 @@
             </form>
              </div>
           </div>
+          <!-- End Filter -->
+          <!-- Content -->
 
           <div class="col-md-9">
             <div class="row">
-              <div class="col-md-6">
-                <div class="product-item">
-                  <a href="job-details.php"><img src="assets/images/product-1-370x270.jpg" alt=""></a>
-                  <div class="down-content">
-                    <a href="job-details.php"><h4>Lorem ipsum dolor sit amet</h4></a>
+            <?php
+            foreach($projects as $project){
+              $ask="";
+              if($project["amountNeeded"] && !$project["consultancyNeeded"]){
+                $ask="Financing";
+              }elseif($project["consultancyNeeded"] && !$project["amountNeeded"]){
+                $ask="Consultancy";
+              }elseif($project["consultancyNeeded"] && $project["amountNeeded"]){
+                $ask="Financing and Consultancy";
+              }
 
-                    <h6>$60 000</h6>
 
-                    <h4><small><i class="fa fa-briefcase"></i> Medical / Health Jobs <br> <strong><i class="fa fa-building"></i> BMI Kings Park Hospital</strong></small></h4>
+              echo  '<div class="col-md-6">';
+              echo '<div class="product-item">';
+              echo '<a href="job-details.php"><img src="assets/images/projects/'.$project["mainImg"].'" alt="Main project image"></a>';
+                  echo '<div class="down-content">';
+                  echo '<a href="job-details.php"><h4>'.$project["title"].'</h4></a>';
 
-                    <small>
-                         <strong title="Posted on"><i class="fa fa-calendar"></i> 15-06-2020</strong> &nbsp;&nbsp;&nbsp;&nbsp;
-                         <strong title="Type"><i class="fa fa-file"></i> Contract</strong> &nbsp;&nbsp;&nbsp;&nbsp;
-                         <strong title="Location"><i class="fa fa-map-marker"></i> London</strong>
-                    </small>
-                  </div>
-                </div>
-              </div>
+                  echo '<h6>Looking for: <strong style="color:#00c389">'.$ask.'</strong></h6>';
 
-              <div class="col-md-6">
-                <div class="product-item">
-                  <a href="job-details.php"><img src="assets/images/product-2-370x270.jpg" alt=""></a>
-                  <div class="down-content">
-                    <a href="job-details.php"><h4>Lorem ipsum dolor sit amet</h4></a>
+                  echo '<h4><small><i class="fa fa-briefcase"></i>  '.$project["category"] .'</small></h4>';
 
-                    <h6>$60 000</h6>
-
-                    <h4><small><i class="fa fa-briefcase"></i> Medical / Health Jobs <br> <strong><i class="fa fa-building"></i> BMI Kings Park Hospital</strong></small></h4>
-
-                    <small>
-                         <strong title="Posted on"><i class="fa fa-calendar"></i> 15-06-2020</strong> &nbsp;&nbsp;&nbsp;&nbsp;
-                         <strong title="Type"><i class="fa fa-file"></i> Contract</strong> &nbsp;&nbsp;&nbsp;&nbsp;
-                         <strong title="Location"><i class="fa fa-map-marker"></i> London</strong>
-                    </small>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md-6">
-                <div class="product-item">
-                  <a href="job-details.php"><img src="assets/images/product-3-370x270.jpg" alt=""></a>
-                  <div class="down-content">
-                    <a href="job-details.php"><h4>Lorem ipsum dolor sit amet</h4></a>
-
-                    <h6>$60 000</h6>
-
-                    <h4><small><i class="fa fa-briefcase"></i> Medical / Health Jobs <br> <strong><i class="fa fa-building"></i> BMI Kings Park Hospital</strong></small></h4>
-
-                    <small>
-                         <strong title="Posted on"><i class="fa fa-calendar"></i> 15-06-2020</strong> &nbsp;&nbsp;&nbsp;&nbsp;
-                         <strong title="Type"><i class="fa fa-file"></i> Contract</strong> &nbsp;&nbsp;&nbsp;&nbsp;
-                         <strong title="Location"><i class="fa fa-map-marker"></i> London</strong>
-                    </small>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md-6">
-                <div class="product-item">
-                  <a href="job-details.php"><img src="assets/images/product-4-370x270.jpg" alt=""></a>
-                  <div class="down-content">
-                    <a href="job-details.php"><h4>Lorem ipsum dolor sit amet</h4></a>
-
-                    <h6>$60 000</h6>
-
-                    <h4><small><i class="fa fa-briefcase"></i> Medical / Health Jobs <br> <strong><i class="fa fa-building"></i> BMI Kings Park Hospital</strong></small></h4>
-
-                    <small>
-                         <strong title="Posted on"><i class="fa fa-calendar"></i> 15-06-2020</strong> &nbsp;&nbsp;&nbsp;&nbsp;
-                         <strong title="Type"><i class="fa fa-file"></i> Contract</strong> &nbsp;&nbsp;&nbsp;&nbsp;
-                         <strong title="Location"><i class="fa fa-map-marker"></i> London</strong>
-                    </small>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md-6">
-                <div class="product-item">
-                  <a href="job-details.php"><img src="assets/images/product-5-370x270.jpg" alt=""></a>
-                  <div class="down-content">
-                    <a href="job-details.php"><h4>Lorem ipsum dolor sit amet</h4></a>
-
-                    <h6>$60 000</h6>
-
-                    <h4><small><i class="fa fa-briefcase"></i> Medical / Health Jobs <br> <strong><i class="fa fa-building"></i> BMI Kings Park Hospital</strong></small></h4>
-
-                    <small>
-                         <strong title="Posted on"><i class="fa fa-calendar"></i> 15-06-2020</strong> &nbsp;&nbsp;&nbsp;&nbsp;
-                         <strong title="Type"><i class="fa fa-file"></i> Contract</strong> &nbsp;&nbsp;&nbsp;&nbsp;
-                         <strong title="Location"><i class="fa fa-map-marker"></i> London</strong>
-                    </small>
-                  </div>
-                </div>
-              </div>
-
-              <div class="col-md-6">
-                <div class="product-item">
-                  <a href="job-details.php"><img src="assets/images/product-6-370x270.jpg" alt=""></a>
-                  <div class="down-content">
-                    <a href="job-details.php"><h4>Lorem ipsum dolor sit amet</h4></a>
-
-                    <h6>$60 000</h6>
-
-                    <h4><small><i class="fa fa-briefcase"></i> Medical / Health Jobs <br> <strong><i class="fa fa-building"></i> BMI Kings Park Hospital</strong></small></h4>
-
-                    <small>
-                         <strong title="Posted on"><i class="fa fa-calendar"></i> 15-06-2020</strong> &nbsp;&nbsp;&nbsp;&nbsp;
-                         <strong title="Type"><i class="fa fa-file"></i> Contract</strong> &nbsp;&nbsp;&nbsp;&nbsp;
-                         <strong title="Location"><i class="fa fa-map-marker"></i> London</strong>
-                    </small>
-                  </div>
-                </div>
-              </div>
-
+                  echo '<small>';
+                  echo '<strong title="Posted on"><i class="fa fa-calendar"></i>  '. date("d.m.Y", strtotime($project["createdAt"])).'</strong> &nbsp;&nbsp;&nbsp;&nbsp;' ;
+                  // echo '<strong title="Type"><i class="fa fa-file"></i> Contract</strong> &nbsp;&nbsp;&nbsp;&nbsp;';
+                  echo '<strong title="Location"><i class="fa fa-map-marker"></i> Location</strong>';
+                  echo '</small>';
+                  echo '</div>';
+                  echo '</div>';
+                  echo '</div>';
+            }
+          ?>
+          <!-- TODO: Handle pagination -->
               <div class="col-md-12">
                 <ul class="pages">
-                  <li><a href="#">1</a></li>
-                  <li class="active"><a href="#">2</a></li>
+                  <li class="active"><a href="#">1</a></li>
+                  <li><a href="#">2</a></li>
                   <li><a href="#">3</a></li>
                   <li><a href="#">4</a></li>
                   <li><a href="#"><i class="fa fa-angle-double-right"></i></a></li>
                 </ul>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-      <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Book Now</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <div class="contact-form">
-              <form action="#" id="contact">
-                  <div class="row">
-                       <div class="col-md-6">
-                          <fieldset>
-                            <input type="text" class="form-control" placeholder="Pick-up location" required="">
-                          </fieldset>
-                       </div>
-
-                       <div class="col-md-6">
-                          <fieldset>
-                            <input type="text" class="form-control" placeholder="Return location" required="">
-                          </fieldset>
-                       </div>
-                  </div>
-
-                  <div class="row">
-                       <div class="col-md-6">
-                          <fieldset>
-                            <input type="text" class="form-control" placeholder="Pick-up date/time" required="">
-                          </fieldset>
-                       </div>
-
-                       <div class="col-md-6">
-                          <fieldset>
-                            <input type="text" class="form-control" placeholder="Return date/time" required="">
-                          </fieldset>
-                       </div>
-                  </div>
-                  <input type="text" class="form-control" placeholder="Enter full name" required="">
-
-                  <div class="row">
-                       <div class="col-md-6">
-                          <fieldset>
-                            <input type="text" class="form-control" placeholder="Enter email address" required="">
-                          </fieldset>
-                       </div>
-
-                       <div class="col-md-6">
-                          <fieldset>
-                            <input type="text" class="form-control" placeholder="Enter phone" required="">
-                          </fieldset>
-                       </div>
-                  </div>
-              </form>
-           </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-            <button type="button" class="btn btn-primary">Book Now</button>
           </div>
         </div>
       </div>
